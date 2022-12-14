a3ffb055f7ef100b117516c93b25ea8a33944b65
Matthew Davidson <mdavidson982@g.rwu.edu>
2022-12-14 12:32:49 -0500

    Functioning fragments and navigation bar. They don't do anything yet, they're just there and when you click them it brings you to a new page.
    I have to stop working on this for now, I need to go to calc then to work.

--- app/src/main/java/com/zybooks/enigmamachine/MainActivity.java
+++ app/src/main/java/com/zybooks/enigmamachine/MainActivity.java
@@ -1,8 +1,14 @@
 package com.zybooks.enigmamachine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+
+            AppBarConfiguration appBarConfig = new AppBarConfiguration.Builder(
+                    R.id.navigation_enigma, R.id.navigation_rotor, R.id.navigation_plugboard)
+                    .build();
+
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
+            NavigationUI.setupWithNavController(navView, navController);
+        }
     }
+
 }
